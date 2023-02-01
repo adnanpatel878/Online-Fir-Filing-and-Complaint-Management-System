ea6581a721f129c4e8e5247b85830f4d6702e794
adnanpatel878 <105789120+adnanpatel878@users.noreply.github.com>
2023-02-01 13:03:21 +0530

    Add files via upload

---
+++ DB/crime_portal.sql
@@ -0,0 +1,251 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 25, 2023 at 04:36 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crime_portal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaint`
+--
+
+CREATE TABLE `complaint` (
+  `c_id` int(11) NOT NULL,
+  `a_no` bigint(12) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `type_crime` varchar(50) NOT NULL,
+  `d_o_c` date NOT NULL,
+  `description` varchar(7000) NOT NULL,
+  `inc_status` varchar(50) DEFAULT 'Unassigned',
+  `pol_status` varchar(50) DEFAULT 'null',
+  `p_id` varchar(50) DEFAULT 't101'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `complaint`
+--
+
+INSERT INTO `complaint` (`c_id`, `a_no`, `location`, `type_crime`, `d_o_c`, `description`, `inc_status`, `pol_status`, `p_id`) VALUES
+(1, 123214521452, 'RR NAGAR', 'Robbery', '2023-01-06', 'My Home has been Robbed.', 'Assigned', 'ChargeSheet Filed', 't101');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `head`
+--
+
+CREATE TABLE `head` (
+  `h_id` varchar(50) NOT NULL,
+  `h_pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `head`
+--
+
+INSERT INTO `head` (`h_id`, `h_pass`) VALUES
+('head@kp', 'head');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `police`
+--
+
+CREATE TABLE `police` (
+  `p_name` varchar(50) NOT NULL,
+  `p_id` varchar(50) NOT NULL,
+  `spec` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `p_pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `police`
+--
+
+INSERT INTO `police` (`p_name`, `p_id`, `spec`, `location`, `p_pass`) VALUES
+('amir', 'a101', 'All', 'KENGRI', 'amir123'),
+('sharukh', 'a102', 'All', 'MG ROAD', 'srk123'),
+('salman', 't101', 'All', 'RR NAGAR', 'salman123'),
+('Akshay', 'y011', 'ALL', 'RR NAGR', '123');
+
+--
+-- Triggers `police`
+--
+DELIMITER $$
+CREATE TRIGGER `deleteLog` AFTER DELETE ON `police` FOR EACH ROW INSERT INTO logs VALUES(null,old.p_id,"Deleted",NOW())
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `police_station`
+--
+
+CREATE TABLE `police_station` (
+  `i_id` varchar(50) NOT NULL,
+  `i_name` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `i_pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `police_station`
+--
+
+INSERT INTO `police_station` (`i_id`, `i_name`, `location`, `i_pass`) VALUES
+('bapu@kengeri', 'Bapu', 'MG ROAD', 'bapu'),
+('rama@rrnagar', 'Rama', 'RR NAGAR', 'rama'),
+('shivam@kengeri', 'Shivam', 'KENGERI', 'shivam');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `update_case`
+--
+
+CREATE TABLE `update_case` (
+  `c_id` int(11) NOT NULL,
+  `d_o_u` timestamp NOT NULL DEFAULT current_timestamp(),
+  `case_update` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `u_name` varchar(50) NOT NULL,
+  `u_id` varchar(50) NOT NULL,
+  `u_pass` varchar(50) NOT NULL,
+  `u_addr` varchar(100) NOT NULL,
+  `a_no` bigint(12) NOT NULL,
+  `gen` varchar(15) NOT NULL,
+  `mob` bigint(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`u_name`, `u_id`, `u_pass`, `u_addr`, `a_no`, `gen`, `mob`) VALUES
+('adnan', 'adnan@gmail.com', 'adnan', 'RR NAGAR', 123214521452, 'Male', 9854123654),
+('het', 'het@gmail.com', 'het', 'MG ROAD', 123456789123, 'M', 8770123456),
+('aaquib', 'aaquib@gmail.com', 'aaquib', 'KENGERI', 623214521459, 'Male', 8853123651);
+
+--
+-- Triggers `user`
+--
+DELIMITER $$
+CREATE TRIGGER `after_insert_trig` AFTER INSERT ON `user` FOR EACH ROW BEGIN
+INSERT INTO user_backup VALUES(NEW.u_name,NEW.u_id,NEW.u_pass,NEW.u_addr,NEW.a_no,NEW.gen,NEW.mob);
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_backup`
+--
+
+CREATE TABLE `user_backup` (
+  `u_name` varchar(50) NOT NULL,
+  `u_id` varchar(50) NOT NULL,
+  `u_pass` varchar(50) NOT NULL,
+  `u_addr` varchar(100) NOT NULL,
+  `a_no` bigint(12) NOT NULL,
+  `gen` varchar(15) NOT NULL,
+  `mob` bigint(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `user_backup`
+--
+
+INSERT INTO `user_backup` (`u_name`, `u_id`, `u_pass`, `u_addr`, `a_no`, `gen`, `mob`) VALUES
+('adnan', 'adnan@gmail.com', 'adnan', 'RR NAGAR', 123214521452, 'Male', 9854123654),
+('het', 'het@gmail.com', 'het', 'MG ROAD', 123456789123, 'M', 8770123456),
+('aaquib', 'aaquib@gmail.com', 'aaquib', 'KENGERI', 623214521459, 'Male', 8853123651);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `complaint`
+--
+ALTER TABLE `complaint`
+  ADD PRIMARY KEY (`c_id`);
+
+--
+-- Indexes for table `police`
+--
+ALTER TABLE `police`
+  ADD PRIMARY KEY (`p_id`);
+
+--
+-- Indexes for table `police_station`
+--
+ALTER TABLE `police_station`
+  ADD PRIMARY KEY (`i_id`),
+  ADD UNIQUE KEY `location` (`location`);
+
+--
+-- Indexes for table `update_case`
+--
+ALTER TABLE `update_case`
+  ADD UNIQUE KEY `d_o_u` (`d_o_u`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`a_no`),
+  ADD UNIQUE KEY `u_id` (`u_id`),
+  ADD UNIQUE KEY `mob` (`mob`);
+
+--
+-- Indexes for table `user_backup`
+--
+ALTER TABLE `user_backup`
+  ADD PRIMARY KEY (`a_no`),
+  ADD UNIQUE KEY `u_id` (`u_id`),
+  ADD UNIQUE KEY `mob` (`mob`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `complaint`
+--
+ALTER TABLE `complaint`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
